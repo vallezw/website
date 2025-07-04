14a8c10464342cbdb1a5f4dcc81e0291049f30d8
Valentin Zwerschke <vallezw@gmail.com>
2025-07-04 06:11:29 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
         <p>
           Currently I&apos;m a working student at <Link href="https://quantco.com"  className={cn(
           "underline underline-offset-2 decoration-foreground/40 font-medium hover:opacity-70 transitions-all"
-        )}>QuantCo</Link> and aspiring to study Music Education at HMTM Munich.
+        )}>QuantCo</Link>, studying Mathematics B.Sc (LMU) and Music Education (HMTM).
         </p>
       </div>
 
